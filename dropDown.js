15d4d61095483ad0a4bc9376c0a413825e3ce157
“y“chenjianguang <jianguangchen@foxmail.com>
2018-04-20 00:33:38 +0800

    增加了元素初始化跟样式初始化

--- dropDown.js
+++ dropDown.js
@@ -4,15 +4,25 @@
         this.dropDown = document.getElementById(dropId);
         this.tip = tip;
         this.tipDiv = null;
+        this.distance = distance;
     }
-    DropDown.prototype.init = function () {
+    DropDown.prototype.initEle = function () {
         this.tipDiv = document.createElement('div');
         this.tipDiv.setAttribute('class', 'tipDiv');
+        this.dropDown.setAttribute('class', 'dropDown')
         this.tipDiv.innerHTML = this.tip;
         let first = this.dropDown.firstChild;
         this.dropDown.insertBefore(this.tipDiv, first);
+    }
+    DropDown.prototype.initCss = function () {
         let styleEle = document.createElement('style');
-        styleEle.innerHTML += '.tipDiv{height:50px;line-height:50px;text-align:center;width:100%;border:1px solid #ccc}';
+        styleEle.innerHTML += '.dropDown .tipDiv{height:50px;line-height:50px;text-align:center;width:100%;border:1px solid #ccc}';
+        styleEle.innerHTML += `.dropDown{width:100%;border:solid 1px blue; transform:translate(0px,-61px)}`;
         document.head.appendChild(styleEle);
+    }
+
+    DropDown.prototype.transform = function (dis) {
+        this.dropDown.webkitTranform = `translate(0,${dis}px)`;
+        this.dropDown.transform = `translate(0,${dis}px)`;
     }
 })();
